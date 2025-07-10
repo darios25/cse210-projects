41e7ae112a7c054cf200140f87dd974890db847b
darios25 <dgiaccone@byupathway.edu>
2025-07-10 23:48:18 +0200

    exercise1complited

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is yuor first name?");
+        string firstname = Console.ReadLine();
+
+        Console.Write("what is your last name?");
+        string lastname = Console.ReadLine();
+
+        // print(f"Your name is {lastname}, {firstname} {lastname}")
+        Console.WriteLine($"your name is {lastname}, {firstname} {lastname} ");
     }
 }

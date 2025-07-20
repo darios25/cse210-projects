af456babc4eab773c04f41d33f85124f51993b3f
darios25 <dgiaccone@byupathway.edu>
2025-07-20 18:22:53 +0200

    Fraction complited

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction();              // 1/1
+        Fraction f2 = new Fraction(5);             // 5/1
+        Fraction f3 = new Fraction(3, 4);          // 3/4
+        Fraction f4 = new Fraction(1, 3);          // 1/3
+
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimalValue());
+
+        Console.WriteLine(f2.GetFractionString());
+        Console.WriteLine(f2.GetDecimalValue());
+
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimalValue());
+
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimalValue());
+
+
+        f1.SetNumerator(6);
+        f1.SetDenominator(7);
+        Console.WriteLine($"New value f1: {f1.GetFractionString()} → {f1.GetDecimalValue()}");
+
     }
 }

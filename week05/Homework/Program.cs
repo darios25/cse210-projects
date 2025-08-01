b47c3a1c99fdab590d8113386285596957c8d383
darios25 <dgiaccone@byupathway.edu>
2025-08-01 19:55:27 +0200

    homework.complited

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment baseAssignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(baseAssignment.GetSummary());
+
+        // Test della classe MathAssignment
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        // Test della classe WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
+
+
 }

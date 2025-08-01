6b728b0e8b14ce18f6eb02861382daee1c89c6f1
darios25 <dgiaccone@byupathway.edu>
2025-08-01 21:54:26 +0200

    homework.complited1

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -7,12 +7,12 @@ class Program
         Assignment baseAssignment = new Assignment("Samuel Bennett", "Multiplication");
         Console.WriteLine(baseAssignment.GetSummary());
 
-        // Test della classe MathAssignment
+        
         MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
         Console.WriteLine(math.GetSummary());
         Console.WriteLine(math.GetHomeworkList());
 
-        // Test della classe WritingAssignment
+        
         WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         Console.WriteLine(writing.GetSummary());
         Console.WriteLine(writing.GetWritingInformation());

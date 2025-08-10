35f6e5685220dcb238f86fa61a0f91d61dc16367
darios25 <dgiaccone@byupathway.edu>
2025-08-10 22:49:55 +0200

    shapes.complited

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 4.0);
+        Rectangle rectangle = new Rectangle("Blue", 5.0, 3.0);
+        Circle circle = new Circle("Green", 2.5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Type: {shape.GetType().Name}");
+            Console.WriteLine($"Color: {shape.GetColor()}");
+            Console.WriteLine($"Area: {shape.GetArea():F2}");
+            Console.WriteLine();
+        }
+
+
     }
 }
